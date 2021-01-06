39d673eba87d4a1c46bb13b245d73be23ae5c520
EDUARDO FRANCA MARINHO <67811267+EduardoFMarinho@users.noreply.github.com>
2021-01-06 17:42:59 -0300

    ServerJS and more functions

---
+++ src/database/fakedata.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-27.222633",
+    lng: "-49.6455874",
+    name: "Lar das meninas",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+
+      "https://images.unsplash.com/photo-1551524164-687a55dd1126?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 18h até 8h",
+    open_on_weekends: "1",
+  },
+  {
+    id: 2,
+    lat: "-27.222633",
+    lng: "-49.6455874",
+    name: "Lar das meninos",
+    description:
+      "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+
+      "https://images.unsplash.com/photo-1551524164-687a55dd1126?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80",
+    ],
+    instructions:
+      "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 18h até 8h",
+    open_on_weekends: "0",
+  },
+];
---
+++ src/pages.js
@@ -0,0 +1,21 @@
+module.exports = require('./database/fakedata.js');
+
+    module.exports = {
+
+        index(req, res) {
+            return res.render('index')
+        },
+
+        orphanage(req, res) {
+            return res.render('orphanage')
+        },
+
+        orphanages(req, res){
+            return res.render('orphanages', { orphanages })
+        },
+
+        createOrphanage(req, res){
+            return res.render('create-orphanage')
+        }
+    
+}
