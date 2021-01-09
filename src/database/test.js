6e540a0c3fec9967ca2b9ad38bf684a9cfbd162b
EDUARDO FRANCA MARINHO <67811267+EduardoFMarinho@users.noreply.github.com>
2021-01-09 19:42:49 -0300

    Fakedata test

---
+++ src/database/test.js
@@ -0,0 +1,30 @@
+const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
+
+Database.then(async db => {
+    // inserir dados na tabela
+
+    await saveOrphanage(db, {
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Local de carinho",
+        description:
+          "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+          "https://images.unsplash.com/photo-1449844908441-8829872d2607?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    
+          "https://images.unsplash.com/photo-1551524164-687a55dd1126?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=925&q=80",
+        ].toString(),
+        instructions:
+          "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "1"
+    })
+
+    // consultar dados da tabela
+    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+    console.log(selectedOrphanages)
+
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+    console.log(orphanage)
+})
