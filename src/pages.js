0f79953ca1121771c760fc4b0846f8d420d46707
EDUARDO FRANCA MARINHO <67811267+EduardoFMarinho@users.noreply.github.com>
2021-01-10 20:13:38 -0300

    Config DB

--- src/pages.js
+++ src/pages.js
@@ -7,14 +7,18 @@ module.exports = {
     return res.render("index");
   },
   async orphanage(req, res) {
+
     const id = req.query.id;
 
     try {
       const db = await Database;
-      const orphanage = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+      const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+      const orphanage = results[0]
 
-      console.log(orphanage[0])
-      return res.render("orphanage")
+      orphanage.images = orphanage.images.split(",")
+      orphanage.firstImage = orphanage.images[0]
+
+      return res.render('orphanage', {orphanage})
     } catch (error) {
       console.log(error);
       return res.send("Erro no banco de dados!");
